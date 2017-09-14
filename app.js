22fa500b0551b3edc3d6ac5d9671f01fc9e389e3
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-09-13 21:48:49 -0500

    testing

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ const bot = dashbotwrap.setDatbot(utils.initBot());
 
 // bot.use(dashbot);
 
-bot.dialog('/', (session) =>
-    session.send(`Hola ${session.message.user.name.split(" ", 1)[0]}, me dijiste: ${session.message.text}`));
+bot.dialog('/', (session) => {
+    session.send(`Hola ${session.message.user.name.split(" ", 1)[0]}, me dijiste: ${session.message.text}`);
+    session.send(JSON.stringify(session.message.user));
+}
+);
 
 
 
