d758831922472bfd2a585fc3330dded81208b671
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-09-15 10:20:47 -0500

    refactoring

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const dashbotwrap = require('./businesslogic/dashbotwrapper');
 
 const bot = dashbotwrap.setDatbot(utils.initBot());
 
-// const dashbot = require('dashbot')(utils.dashbotApiMap).microsoft;
-
-
-// bot.use(dashbot);
-
 bot.dialog('/', (session) => {
     session.send(`Hola ${session.message.user.name.split(" ", 1)[0]}, me dijiste: ${session.message.text}`);
     session.send(JSON.stringify(session.message.user));
