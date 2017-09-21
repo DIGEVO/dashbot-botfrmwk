996000f5393ed8154ca4cf40f8b75890e707f664
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-09-21 16:24:23 -0500

    asdsd

--- businesslogic/conversationflow.js
+++ businesslogic/conversationflow.js
@@ -9,18 +9,36 @@ const cache = new NodeCache({ stdTTL: process.env.TTL || 7200 });
 
 module.exports = {
     firstStep(session, args, next) {
-        session.dialogData.activeBot = true;
         const channelId = session.message.address.channelId;
         const userId = session.message.user.id;
+        session.dialogData.isMsgFromWebhook = true;
 
-        if (channelId !== 'directline' || userId !== 'DashbotChannel') next();
+        if (channelId !== 'directline' || userId !== 'DashbotChannel') {
+            cache.set(userId, session.message.address);
+            session.dialogData.isMsgFromWebhook = false;
+        }
 
+        next();
+    },
+
+    secondStep(session, args, next) {
+        if (session.dialogData.isMsgFromWebhook) {
+            module.exports.sendMessage(session);
+            next();
+        }
+        else {
+            session.beginDialog('/prueba');
+        }
+    },
+
+    finalStep(session, args, next) {
+        session.endDialog();
+    },
+
+    sendMessage(session) {
         const msg = JSON.parse(session.message.text);
         console.log(`1: ${msg.userId}`);
-        const userCachedData = cache.get(msg.userId) || { paused: false, address: undefined };
-
-        userCachedData.paused = msg.paused;
-        cache.set(msg.userId, userCachedData);
+        const address = cache.get(msg.userId);
 
         let errorMsg = undefined;
         const name = utils.getName(session.message);
@@ -31,8 +49,8 @@ module.exports = {
                 `Hola ${name}, ${greetting}, a partir de este momento hablarás con una persona` :
                 `Hola ${name}, ${greetting}, a partir de este momento hablarás con la plataforma`);
 
-        if (userCachedData.address) {
-            bot.send(new builder.Message().text(text).address(userCachedData.address));
+        if (address) {
+            bot.send(new builder.Message().text(text).address(address));
         } else {
             errorMsg = `Error: No se pudo enviar el mensaje: "${msg.text}" ` +
                 `al cliente "${msg.userId}" porque la dirección del mismo no aparece en la base de datos.`;
@@ -40,22 +58,19 @@ module.exports = {
         }
 
         session.send(errorMsg || (msg.text ? 'Mensaje enviado.' : 'Detención/Activación del bot.'));
-        session.dialogData.activeBot = false;
-    },
-
-    secondStep(session, args, next) {
-        console.log('-*-*--*-*-* 1');
-        if (session.dialogData.activeBot) next();
-        console.log('-*-*--*-*-* 2');
-        //console.log(`2: ${userId}`);
-       // cache.set(userId, { paused: false, address: session.message.address });
-        session.beginDialog('/prueba');
-        console.log('-*-*--*-*-* 3');
-    },
-
-    finalStep(session, args, next){
-        session.endDialog();
     }
+
+    // thirdStep(session, args, next) {
+    //     console.log('-*-*--*-*-* 1');
+    //     if (session.dialogData.activeBot) next();
+    //     console.log('-*-*--*-*-* 2');
+    //     //console.log(`2: ${userId}`);
+    //     // cache.set(userId, { paused: false, address: session.message.address });
+    //     session.beginDialog('/prueba');
+    //     console.log('-*-*--*-*-* 3');
+    // },
+
+
     //,
 
     // proxy: (session, args, next) => {
