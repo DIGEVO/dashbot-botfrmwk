00e119a5589a5a546e19f3072dea13e512b25cd0
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-10-31 17:06:28 -0500

    testing1

--- dashbot/index.js
+++ dashbot/index.js
@@ -7,8 +7,9 @@ const dashbot = require('dashbot')(process.env.DASHBOT_API_KEY_GENERIC).generic;
 const self = module.exports = {
     dashbotMessage: (text, userId, conversationId) => ({
         "text": text,
-        "userId": userId,
-        "conversationId": conversationId
+        "userId": userId
+        // ,
+        // "conversationId": conversationId
     }),
 
     logMessage: (text, userId, conversationId, incoming = true) =>
